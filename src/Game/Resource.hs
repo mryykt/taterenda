afd3a8d24ff779e8ed002cef2fda6696479111ef
mryykt <avrpic9@gmail.com>
2024-10-06 14:47:36 +0900

    feat: リソースローダ

--- src/Game/Resource.hs
+++ src/Game/Resource.hs
@@ -1,3 +1,22 @@
-module Game.Resource where
+module Game.Resource (Loader, get, soundLoader) where
 
+import Control.Concurrent.Async (Async, async, mapConcurrently, poll)
+import Raylib.Core.Audio (loadSound)
 import Raylib.Types.Core.Audio (Sound)
+
+newtype Loader t a = Loader (Async (t a))
+
+loader :: (Traversable t) => (x -> IO a) -> t x -> IO (Loader t a)
+loader f res = do
+  th <- async (mapConcurrently f res)
+  return $ Loader th
+
+get :: Loader t a -> IO (Maybe (t a))
+get (Loader l) = do
+  result <- poll l
+  case result of
+    Just (Right x) -> return $ Just x
+    _ -> return Nothing
+
+soundLoader :: (Traversable t) => t String -> IO (Loader t Sound)
+soundLoader = loader loadSound
--- src/Game/Types.hs
+++ src/Game/Types.hs
@@ -1,10 +1,16 @@
 module Game.Types (module Game.Types) where
 
+import Data.IntMap (IntMap)
+import Game.Resource (Loader)
 import Lens.Micro.TH (makeLenses)
+import Raylib.Types (Sound)
 import Raylib.Util (WindowResources)
+import Tateren.Types (Tateren)
 
 data Game = Game {_window :: WindowResources, _appState :: AppState}
 
-data AppState = AppState
+data AppState = LoadState Load
+
+data Load = Load {_tateren :: Tateren, sounds :: Loader IntMap Sound}
 
 makeLenses ''Game
