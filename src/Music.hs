98d8ab3a22be97153d070933ccc838bd1298b9e7
mryykt <avrpic9@gmail.com>
2024-10-10 19:49:44 +0900

    add: difficulty for chart

--- src/Music.hs
+++ src/Music.hs
@@ -1,4 +1,4 @@
-module Music (Music, MusicList, name, genre, artist, bpm, directory, chart, sounds, current, prev, next, selectAnother, selectNormal, list) where
+module Music (Music, MusicList, name, genre, artist, bpm, difficulty, directory, chart, sounds, current, prev, next, selectAnother, selectNormal, list) where
 
 import Data.IntMap (IntMap)
 import Data.Tuple.Extra (second3)
@@ -13,6 +13,7 @@ data Music = Music
   , genre :: String
   , artist :: String
   , bpm :: Float
+  , difficulty :: Maybe Int
   , directory :: FilePath
   , chart :: FilePath
   , sounds :: IntMap FilePath
@@ -64,26 +65,26 @@ list =
   )
 
 frozen, zionia, grave, graveA, abacyber, platinum, llr, ld, empire, empireA, a3, a3A, a2, a2A, ark, arkA, wanderer, witchcraft, witchcraftA, l9, _2002, _2002A, binary :: Music
-frozen = Music "Frozen Bond" "ballade" "paraoka" 106 "frozen" "frozen.ttr" Sounds.frozen
-zionia = Music "Zionia Garden" "progressive" "paraoka" 172 "zionia" "zionia.ttr" Sounds.frozen
-grave = Music "rainy graveyard" "fold groove" "paraoka" 115 "grave" "grave.ttr" Sounds.grave
-graveA = Music "rg (autumn-taste)" "folk groove" "paraoka/doctorR" 115 "grave" "grave_a.ttr" Sounds.graveA
-abacyber = Music "" "digi-rock" "remixed Paraoka" 155 "abacyber" "abacyber.ttr" Sounds.abacyber
-platinum = Music "platinum garden" "breakbeats" "paraoka" 152 "platinum" "platinum.ttr" Sounds.platinum
-llr = Music "LLR" "E-J" "paraoka" 170 "llr" "llr.ttr" Sounds.llr
-ld = Music "lastdance" "fusion" "paraoka" 112 "ld" "ld.ttr" Sounds.ld
-empire = Music "empire of the steel" "minimal" "paraoka" 153 "empire" "empire.ttr" Sounds.empire
-empireA = Music "empire of the steam" "techno" "paraoka" 153 "empire" "empire_a.ttr" Sounds.empireA
-a3 = Music "A3" "speedfunk" "paraoka" 147 "a3" "a3.ttr" Sounds.a3
-a3A = Music "AcidAce-Alliance(deep mix)" "house" "paraoka Remixed by doctor.R" 131 "a3" "a3_a.ttr" Sounds.a3A
-a2 = Music "A2" "psychedelic rock" "paraoka" 156 "a2" "a2.ttr" Sounds.a2
-a2A = Music "A2(sprit flow mix)" "psychedelic punk" "paraoka Remixed by doctor.R" 156 "a2" "a2_a.ttr" Sounds.a2A
-ark = Music "Ark" "E-J" "paraoka" 160 "ark" "ark.ttr" Sounds.ark
-arkA = Music "Ark(acid mix)" "E-J" "paraoka/doctorR" 168 "ark" "ark_a.ttr" Sounds.arkA
-wanderer = Music "wanderer's destiny" "rock" "paraoka" 122 "wanderer" "wanderer.ttr" Sounds.wanderer
-witchcraft = Music "witchcraft" "bigbeat" "paraoka" 115 "witchcraft" "witchcraft.ttr" Sounds.witchcraft
-witchcraftA = Music "witchcraft(another)" "bigbeat(7key)" "paraoka" 115 "witchcraft" "witchcraft_a.ttr" Sounds.witchcraftA
-l9 = Music "L9" "Progressive" "paraoka" 123 "l9" "l9.ttr" Sounds.l9
-_2002 = Music "2002,november" "&" "dj nagureo / Yamajet" 128 "2002nov" "2002nov.ttr" Sounds._2002
-_2002A = Music "2002,november (softlanding mix)" "&" "dj nagureo / Yamajet" 2002 "2002nov" "2002nov_a.ttr" Sounds._2002A
-binary = Music "Binary Numbers" "Digital Techno" "Cyclone Area" 94 "binary" "binary.ttr" Sounds.binary
+frozen = Music "Frozen Bond" "ballade" "paraoka" 106 (Just 1) "frozen" "frozen.ttr" Sounds.frozen
+zionia = Music "Zionia Garden" "progressive" "paraoka" 172 (Just 2) "zionia" "zionia.ttr" Sounds.frozen
+grave = Music "rainy graveyard" "fold groove" "paraoka" 115 (Just 2) "grave" "grave.ttr" Sounds.grave
+graveA = Music "rg (autumn-taste)" "folk groove" "paraoka/doctorR" 115 Nothing "grave" "grave_a.ttr" Sounds.graveA
+abacyber = Music "" "digi-rock" "remixed Paraoka" 155 (Just 3) "abacyber" "abacyber.ttr" Sounds.abacyber
+platinum = Music "platinum garden" "breakbeats" "paraoka" 152 (Just 3) "platinum" "platinum.ttr" Sounds.platinum
+llr = Music "LLR" "E-J" "paraoka" 170 (Just 3) "llr" "llr.ttr" Sounds.llr
+ld = Music "lastdance" "fusion" "paraoka" 112 (Just 4) "ld" "ld.ttr" Sounds.ld
+empire = Music "empire of the steel" "minimal" "paraoka" 153 (Just 4) "empire" "empire.ttr" Sounds.empire
+empireA = Music "empire of the steam" "techno" "paraoka" 153 Nothing "empire" "empire_a.ttr" Sounds.empireA
+a3 = Music "A3" "speedfunk" "paraoka" 147 (Just 4) "a3" "a3.ttr" Sounds.a3
+a3A = Music "AcidAce-Alliance(deep mix)" "house" "paraoka Remixed by doctor.R" 131 Nothing "a3" "a3_a.ttr" Sounds.a3A
+a2 = Music "A2" "psychedelic rock" "paraoka" 156 (Just 5) "a2" "a2.ttr" Sounds.a2
+a2A = Music "A2(sprit flow mix)" "psychedelic punk" "paraoka Remixed by doctor.R" 156 Nothing "a2" "a2_a.ttr" Sounds.a2A
+ark = Music "Ark" "E-J" "paraoka" 160 (Just 5) "ark" "ark.ttr" Sounds.ark
+arkA = Music "Ark(acid mix)" "E-J" "paraoka/doctorR" 168 Nothing "ark" "ark_a.ttr" Sounds.arkA
+wanderer = Music "wanderer's destiny" "rock" "paraoka" 122 (Just 6) "wanderer" "wanderer.ttr" Sounds.wanderer
+witchcraft = Music "witchcraft" "bigbeat" "paraoka" 115 (Just 6) "witchcraft" "witchcraft.ttr" Sounds.witchcraft
+witchcraftA = Music "witchcraft(another)" "bigbeat(7key)" "paraoka" 115 Nothing "witchcraft" "witchcraft_a.ttr" Sounds.witchcraftA
+l9 = Music "L9" "Progressive" "paraoka" 123 Nothing "l9" "l9.ttr" Sounds.l9
+_2002 = Music "2002,november" "&" "dj nagureo / Yamajet" 128 Nothing "2002nov" "2002nov.ttr" Sounds._2002
+_2002A = Music "2002,november (softlanding mix)" "&" "dj nagureo / Yamajet" 2002 Nothing "2002nov" "2002nov_a.ttr" Sounds._2002A
+binary = Music "Binary Numbers" "Digital Techno" "Cyclone Area" 94 Nothing "binary" "binary.ttr" Sounds.binary
