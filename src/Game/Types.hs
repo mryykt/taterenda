6c6ab2bcfac25d0c22b13040afab851ad9cf7ed0
mryykt <avrpic9@gmail.com>
2024-10-10 12:32:31 +0900

    chore: 改行した

--- src/Game/Types.hs
+++ src/Game/Types.hs
@@ -12,7 +12,15 @@ import Raylib.Types (Sound)
 import Raylib.Util (WindowResources)
 import Tateren.Types (Tateren)
 
-data Game = Game {_window :: WindowResources, _config :: Config, _close :: Bool, _drawer :: (Texture -> Vector -> Rectangle -> IO (), String -> Vector -> Bool -> IO ()), _musicList :: MusicList, _textures :: Textures, _appState :: AppState}
+data Game = Game
+  { _window :: WindowResources
+  , _config :: Config
+  , _close :: Bool
+  , _drawer :: (Texture -> Vector -> Rectangle -> IO (), String -> Vector -> Bool -> IO ())
+  , _musicList :: MusicList
+  , _textures :: Textures
+  , _appState :: AppState
+  }
 
 data Textures = Textures
   { font :: Texture
