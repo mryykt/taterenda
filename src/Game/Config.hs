913837d5ff75a8f0376b09bab040e237a5f1a2a6
mryykt <avrpic9@gmail.com>
2024-10-22 20:42:42 +0900

    feat: キー設定をConfigに追加

--- src/Game/Config.hs
+++ src/Game/Config.hs
@@ -1,26 +1,63 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Game.Config (Config (..), read, write) where
 
 import Control.Monad.Extra (ifM)
 import Data.Aeson.Micro
+  ( FromJSON (..)
+  , ToJSON (..)
+  , Value (Object)
+  , decodeStrict
+  , encodeStrict
+  , object
+  , (.:)
+  , (.=)
+  )
 import qualified Data.ByteString as BS
 import Data.Maybe (fromMaybe)
+import qualified Data.Text as Text
 import Raylib.Core (fileExists)
+import Raylib.Types (KeyboardKey (..))
 import Prelude hiding (read)
+import qualified Prelude
 
 data Config = Config
   { width :: Int
   , height :: Int
   , fullScreen :: Bool
+  , scratchKey :: KeyboardKey
+  , key1 :: KeyboardKey
+  , key2 :: KeyboardKey
   , actualWidth :: Float
   , actualHeight :: Float
   }
 
+deriving instance Read KeyboardKey
+
 instance FromJSON Config where
-  parseJSON (Object v) = Config <$> v .: "width" <*> v .: "height" <*> v .: "fullscreen" <*> return 0 <*> return 0
+  parseJSON (Object v) =
+    Config
+      <$> v .: "width"
+      <*> v .: "height"
+      <*> v .: "fullscreen"
+      <*> fmap (Prelude.read . Text.unpack) (v .: "scratch_key")
+      <*> fmap (Prelude.read . Text.unpack) (v .: "key1")
+      <*> fmap (Prelude.read . Text.unpack) (v .: "key2")
+      <*> return 0
+      <*> return 0
   parseJSON _ = fail "parse Config failed"
 
 instance ToJSON Config where
-  toJSON config = object ["width" .= config.width, "height" .= config.height, "fullscreen" .= config.fullScreen]
+  toJSON config =
+    object
+      [ "width" .= config.width
+      , "height" .= config.height
+      , "fullscreen" .= config.fullScreen
+      , "scratch_key" .= Text.pack (show config.scratchKey)
+      , "key1" .= Text.pack (show config.key1)
+      , "key2" .= Text.pack (show config.key2)
+      ]
 
 write :: Config -> IO ()
 write = BS.writeFile "config.json" . encodeStrict
@@ -38,6 +75,9 @@ def =
     { width = 640
     , height = 480
     , fullScreen = False
+    , scratchKey = KeyLeftShift
+    , key1 = KeyZ
+    , key2 = KeyX
     , actualWidth = 0
     , actualHeight = 0
     }
